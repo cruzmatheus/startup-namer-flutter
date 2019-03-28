b83e2cf637fdb09a271afe41ee4369ad145effa9
Matheus Cruz <contato.mscruz@gmail.com>
2019-03-28 23:16:30 +0000

    Refactoring the build method of RandomWordState Widget to proper fill the heart icon

--- lib/main.dart
+++ lib/main.dart
@@ -14,39 +14,43 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = new Set();
+  
   @override
   Widget build(BuildContext context) {
-    final _suggestions = <WordPair>[];
-    final _biggerFont = const TextStyle(fontSize: 18.0);
-    final Set<WordPair> _saved = new Set();
-
-    Widget _buildRow(WordPair pair) {
-      final bool _alreadySaved = _saved.contains(pair);
-      return ListTile(
-        title: Text(pair.asPascalCase, style: _biggerFont),
-        trailing: new Icon(_alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: _alreadySaved ? Colors.red : null),
-      );
-    }
-
-    Widget _buildSuggestions() {
-      return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-          final index = i ~/ 2;
-          if (index >=_suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        }
-      );
-    }
-
-
     return Scaffold(
       appBar: AppBar(title: Text('Startup Name Generator')),
       body: _buildSuggestions());
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool _alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(pair.asPascalCase, style: _biggerFont),
+      trailing: new Icon(_alreadySaved ? Icons.favorite : Icons.favorite_border, color: _alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (_alreadySaved) _saved.remove(pair);
+          else _saved.add(pair);
+        });
+      },
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+        final index = i ~/ 2;
+        if (index >=_suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
   }
 }
 
